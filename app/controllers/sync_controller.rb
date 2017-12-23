44d3ed7a19e9f5c7c6ed4799cf02ad0ca62d883b
John Brunton <jbrunton@zipcar.com>
2017-12-23 17:06:22 +0000

    show statements on sync preview

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -2,7 +2,7 @@ class SyncController < ApplicationController
   before_action :set_project, only: [:preview, :sync]
 
   def preview
-
+    @statements = @project.scan
   end
 
 private
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+  def scan
+    scan_dir = File.join(directory, 'statements')
+    Dir.glob("#{scan_dir}/*.csv").map do |filename|
+      Statement.new(filename: File.basename(filename))
+    end
+  end
 end
