a9d1b8e254304ce132d22383fbf0416f4ad37599
John Brunton <jbrunton@zipcar.com>
2017-12-24 12:07:02 +0000

    add data_attributes method to Transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,18 +3,13 @@ require 'digest'
 class Transaction < ApplicationRecord
   belongs_to :project
 
+  DATA_ATTRIBUTES = ['date', 'transaction_type', 'description', 'value', 'balance']
+
   def to_s
     "id: #{id}, #{date}, #{description}, #{category}"
   end
-  #
-  # def self.local
-  #   transactions = []
-  #   Dir.glob("#{Rails.root}/transactions/*.csv").each do |file|
-  #     puts "Found: #{file}"
-  #     Parser.read(file).map do |attrs|
-  #       transactions << Transaction.new(attrs)
-  #     end
-  #   end
-  #   transactions
-  # end
+
+  def data_attributes
+    attributes.slice(*DATA_ATTRIBUTES)
+  end
 end
