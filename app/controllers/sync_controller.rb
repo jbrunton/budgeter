fe003317fd55839fccb13a518722d6fe6857fca4
John Brunton <jbrunton@zipcar.com>
2017-12-24 15:52:58 +0000

    sync transactions

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -7,39 +7,24 @@ class SyncController < ApplicationController
   before_action :set_project, only: [:preview, :sync]
 
   def preview
-    stored_transactions = @project.transactions.to_a
-    statement_transactions = @project.scan_statement_transactions
+    diff = diff_transactions
 
-    @new_transactions = []
-    @orphaned_transactions = stored_transactions.clone
-    @synced_transactions = []
-
-    statement_transactions.each do |candidate|
-      store_index = stored_transactions.index{ |t| t.data_attributes == candidate.data_attributes }
-      orphan_index = @orphaned_transactions.index{ |t| t.data_attributes == candidate.data_attributes }
-      if store_index.nil?
-        @new_transactions << candidate
-      else
-        @orphaned_transactions.delete_at(orphan_index)
-        @synced_transactions << candidate
-      end
-    end
+    @new_transactions = diff[:new_transactions]
+    @orphaned_transactions = diff[:orphaned_transactions]
+    @synced_transactions = diff[:synced_transactions]
   end
 
   def sync
-    @project.transactions.delete_all
-    @statements = @project.scan_statements
-    @statements.each do |statement|
-      statement.scan_transactions.each do |transaction|
-        transaction.save
-      end
-    end
+    diff = diff_transactions
+
+    diff[:orphaned_transactions].each{ |t| t.delete }
+    diff[:new_transactions].each{ |t| t.save }
 
     Dir.glob(File.join(@project.directory, 'transactions/*.yaml')).each do |filename|
       FileUtils.rm(filename)
     end
 
-    @project.transactions.group_by{ |t| t.date.strftime('%Y-%m-%b') }.each do |filename, transactions|
+    @project.transactions.reload.group_by{ |t| t.date.strftime('%Y-%m-%b') }.each do |filename, transactions|
       content = {
         'transactions' => transactions.map { |transaction| marshal_for_file(transaction) }
       }
@@ -58,6 +43,33 @@ private
     attrs = transaction.data_attributes
     attrs['value'] = currency(attrs['value'])
     attrs['balance'] = currency(attrs['balance'])
+    attrs['category'] = transaction.category
     attrs
+  end
+
+  def diff_transactions
+    stored_transactions = @project.transactions.to_a
+    statement_transactions = @project.scan_statement_transactions
+
+    new_transactions = []
+    orphaned_transactions = stored_transactions.clone
+    synced_transactions = []
+
+    statement_transactions.each do |candidate|
+      store_index = stored_transactions.index{ |t| t.data_attributes == candidate.data_attributes }
+      orphan_index = orphaned_transactions.index{ |t| t.data_attributes == candidate.data_attributes }
+      if store_index.nil?
+        new_transactions << candidate
+      else
+        orphaned_transactions.delete_at(orphan_index)
+        synced_transactions << candidate
+      end
+    end
+
+    {
+      new_transactions: new_transactions,
+      orphaned_transactions: orphaned_transactions,
+      synced_transactions: synced_transactions
+    }
   end
 end
