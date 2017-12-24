5241e9b56146877339e62c8f93e11029412984d8
John Brunton <jbrunton@zipcar.com>
2017-12-24 12:08:32 +0000

    add methods to scan statements and stored transactions

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,11 +2,24 @@ class Project < ApplicationRecord
   has_many :transactions
   has_many :statements
 
-  def scan
+  def scan_statements
     scan_dir = File.join(directory, 'statements')
     Dir.glob("#{scan_dir}/*.csv").map do |filename|
       statements.build(filename: File.basename(filename))
     end
+  end
+
+  def scan_stored_transactions
+    stored_transactions = []
+    Dir.glob(File.join(directory, 'transactions/*.yaml')).each do |filename|
+      content = YAML.load_file(filename)
+      if content['transactions']
+        content['transactions'].each do |attrs|
+          stored_transactions << transactions.build(attrs)
+        end
+      end
+    end
+    stored_transactions
   end
 
   def categories
