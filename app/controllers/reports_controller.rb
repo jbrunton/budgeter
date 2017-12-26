0b9a6750e4fdf7d5dae4db40b8b99b5a9a78b502
John Brunton <jbrunton@zipcar.com>
2017-12-26 14:55:33 +0000

    simple spend report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -17,18 +17,11 @@ class ReportsController < ApplicationController
       row = [current_month.strftime('%b %Y')]
       categories.each do |category, _|
         month_spend = @project.transactions.within_month(current_month).where(predicted_category: category).sum(:value)
-        row << month_spend.to_f
+        row << (month_spend < 0 ? -month_spend.to_f : 0)
       end
       @data << row
       current_month = next_month
     end
-    # @data = [
-    #   ['Month',  'Rotten Tomatoes', 'IMDB'],
-    #   ['Alfred Hitchcock (1935)', 8.4,         7.9],
-    #   ['Ralph Thomas (1959)',     6.9,         6.5],
-    #   ['Don Sharp (1978)',        6.5,         6.4],
-    #   ['James Hawes (2008)',      4.4,         6.2]
-    # ]
   end
 
 private
