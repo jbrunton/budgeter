a9095f47249db3fc8266768e3fed9a22f3f05817
John Brunton <jbrunton@zipcar.com>
2017-12-26 14:50:30 +0000

    minor fixes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,14 +10,14 @@ class ReportsController < ApplicationController
       .sum(:value)
       .sort_by { |_, v| v }
       .to_h
-    
+
     @data = [['Month'].concat(categories.map{ |k, _| k })]
     while current_month < last_date
       next_month = current_month.next_month
       row = [current_month.strftime('%b %Y')]
       categories.each do |category, _|
         month_spend = @project.transactions.within_month(current_month).where(predicted_category: category).sum(:value)
-        row << -month_spend.to_f
+        row << month_spend.to_f
       end
       @data << row
       current_month = next_month
