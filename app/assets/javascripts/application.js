b9340a066bbcbd764d87cf95b80b59afc16a343b
John Brunton <jbrunton@zipcar.com>
2017-12-28 15:11:04 +0000

    streamline account management

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require jquery
 //= require materialize
 //= require_tree .
+
+$(document).ready(function() {
+  $('select').material_select();
+});
