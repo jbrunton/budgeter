3e596ecb2c45ade04deb0b13b194dcf3f69ac127
John Brunton <jbrunton@zipcar.com>
2018-02-03 22:44:02 +0000

    extract some methods

--- app/models/csv_statement_parser.rb
+++ app/models/csv_statement_parser.rb
@@ -12,16 +12,9 @@ class CsvStatementParser
     header = csv_data.shift
     header_map = header.map{ |x| x.strip }.each_with_index.to_h
 
-    validate(csv_data, header_map)
+    validate_account(csv_data, header_map)
 
-    candidate_transactions = csv_data.map do |row|
-      @account.transactions.build({
-        date: row[header_map['Date']],
-        description: row[header_map['Description']],
-        value: row[header_map['Value']],
-        balance: row[header_map['Balance']]
-      })
-    end
+    candidate_transactions = build_candidate_transactions(csv_data, header_map)
 
     if @account.account_type == 'credit_card'
       last_transaction = candidate_transactions.pop
@@ -32,25 +25,44 @@ class CsvStatementParser
         raise "Error: expected last transaction to be the account balance."
       end
 
-      candidate_transactions.reverse.each do |t|
-        t.balance = current_balance
-        current_balance -= t.value
-      end
+      update_balances(candidate_transactions, current_balance)
     else
-      candidate_transactions.each do |t|
-        if t.balance.nil?
-          raise "Error: expected balance for all transactions."
-        end
-      end
+      validate_balances(candidate_transactions)
     end
 
     TransactionImporter.new(@account).import(candidate_transactions)
   end
 
 private
-  def validate(csv_data, header_map)
+  def validate_account(csv_data, header_map)
     if csv_data.map{ |row| row[header_map['Account Name']] }.uniq.length > 1
       raise "Please import transactions for one account at a time."
+    end
+  end
+
+  def validate_balances(candidate_transactions)
+    candidate_transactions.each do |t|
+      if t.balance.nil?
+        raise "Error: expected balance for all transactions."
+      end
+    end
+  end
+
+  def build_candidate_transactions(csv_data, header_map)
+    csv_data.map do |row|
+      @account.transactions.build({
+        date: row[header_map['Date']],
+        description: row[header_map['Description']],
+        value: row[header_map['Value']],
+        balance: row[header_map['Balance']]
+      })
+    end
+  end
+
+  def update_balances(candidate_transactions, current_balance)
+    candidate_transactions.reverse.each do |t|
+      t.balance = current_balance
+      current_balance -= t.value
     end
   end
 end
